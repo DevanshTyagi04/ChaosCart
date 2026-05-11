ef6e658e03f8f7f05ea04df1fb5e7fab619ab2be
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2026-05-12 00:57:33 +0530

    Update frontend API URLs for EC2 deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const USERS_API = axios.create({
-  baseURL: 'http://localhost:4001/api/users'
+  baseURL: 'http://13.234.231.164:4001/api/users'
 });
 
 export const PRODUCTS_API = axios.create({
-  baseURL: 'http://localhost:4002/api/products'
+  baseURL: 'http://13.234.231.164:4002/api/products'
 });
 
 export const ORDERS_API = axios.create({
-  baseURL: 'http://localhost:4003/api/orders'
+  baseURL: 'http://13.234.231.164:4003/api/orders'
 });
