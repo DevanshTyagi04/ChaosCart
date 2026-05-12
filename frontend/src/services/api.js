90d0331155065f833752d0d01cf8139699689d66
DevanshTyagi04 <devansh.tyagi04@gmail.com>
2026-05-12 12:20:36 +0530

    Configure frontend for reverse proxy routing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const USERS_API = axios.create({
-  baseURL: 'http://13.234.231.164:4001/api/users'
+  baseURL: '/api/users'
 });
 
 export const PRODUCTS_API = axios.create({
-  baseURL: 'http://13.234.231.164:4002/api/products'
+  baseURL: '/api/products'
 });
 
 export const ORDERS_API = axios.create({
-  baseURL: 'http://13.234.231.164:4003/api/orders'
+  baseURL: '/api/orders'
 });
